df4b26dab98edecccdfa4581ef8e3d1dcebf1ac8
wlstjdwkd <xkakrlfh@naver.com>
2023-05-01 17:18:13 +0900

    Date:23.05.01 doing practice2

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +28,75 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        	
+        	String line = br.readLine();
+        	if(line == null) { return; }
+        	
+        	String firstLine = line;
+        	byte[] body = null;
+        	
+        	
+        	// HTTP 요청 정보 전체 출력 start---------------------------------------------------------------------
+        	System.out.println("HTTP 요청 정보 전체 출력 start---------------------------------------------------------------------");
+        	while(!"".equals(line)) {
+        		System.out.println(line);
+        		line = br.readLine();
+        	}
+        	System.out.println("HTTP 요청 정보 전체 출력 end-----------------------------------------------------------------------");
+        	// HTTP 요청 정보 전체 출력 end-----------------------------------------------------------------------
+        	
+        	
+        	// index.html로 이동 start------------------------------------------------------------------------
+        	// if문 1번째 방법 start--------------------------------------------------------
+        	// '?'로 조건을 걸었을 경우, 쿼리스트링이 없을 때에 화면 전환이 자유롭다.
+        	if(!firstLine.contains("?")) {
+	        	String[] tokens = firstLine.split(" ");
+	        	String url = tokens[1];
+	        		
+	        	body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        	}
+        	// if문 1번째 방법 end----------------------------------------------------------
+        	// if문 2번째 방법 start--------------------------------------------------------
+        	// 문제 발생.......
+        	// 특정 html로 조건을 걸었을 경우, 모든 html에 대해 조건을 걸어 주어야 한다.
+//        	String[] tokens = firstLine.split(" ");
+//        	String url = tokens[1];
+//        	
+//        	if(url.equals("/index.html")) {
+//        		body = Files.readAllBytes(new File("./webapp" + url).toPath());
+//        	}
+        	// if문 2번째 방법 end----------------------------------------------------------
+        	// index.html로 이동 end--------------------------------------------------------------------------
+        	
+        	
+        	// GET방식 회원가입 start----------------------------
+        	if(firstLine.contains("?")) {
+        		String[] tokens = firstLine.split(" ");
+	        	String url = tokens[1];
+	        	
+	        	String requestPath = url.split("[?]")[0];
+	        	String params = url.split("[?]")[1];
+	        	
+	        	System.out.println("requestPath : " + requestPath);
+	        	System.out.println("params      : " + params);
+        	}
+        	// GET방식 회원가입 end------------------------------
+        	
+        	
+        	//
+//        	while(!"".equals(line)) {
+//        		String[] tokens = line.split(" ");
+//        		for(String token : tokens) { System.out.println(token); }
+//        		line = br.readLine();
+//        	}
+        	
+        	
+        		
+        	// -------------------------------------------------
+        		
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+//            byte[] body = "Hello World".getBytes();	// 구조상 재정의.
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
